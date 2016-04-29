e0045bd6ca769c668b928379a11a91d433b17e31
Jared Stafford <jspenguin@jspenguin.org>
2016-04-28 19:31:15 -0500

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# tomb_raider_vpn_fix
+# Tomb Raider VPN Fix
+
+This is an LD_PRELOAD script that forces getifaddrs to return no addresses.
+
+Tomb Raider for Linux won't start if there are any VPNs active that use TUN
+devices. This is because it makes incorrect assumptions about the return value
+of getifaddrs(). The only reason it calls getifaddrs() is to get the MAC address
+for a Version 1 UUID, and falls back to a different algorithm if getifaddrs()
+returns no interfaces at all.
+
+To install, just run the install script. It will install the library in
+~/.local/lib, and show instructions on how to set up Steam to launch Tomb Raider
+with the fix in place.
---
+++ tomb_raider_vpn_fix.c
@@ -0,0 +1,27 @@
+/*
+ * Simple LD_PRELOAD library to fix assumptions made by Tomb Raiders about the output of getifaddrs()
+ *
+ * Compiling:
+ * gcc -m32 -fPIC -shared tomb_raider_vpn_fix.c -o tomb_raider_vpn_fix.so -ldl -Wall
+ *
+ * Installing:
+ * Copy tomb_raider_vpn_fix.so to Tomb Raider dir (.../SteamApps/common/Tomb Raider/bin) then
+ * change steam launch options for Tomb Raider to:
+ *
+ * env LD_PRELOAD=./tomb_raider_vpn_fix.so %command%
+ *
+ */
+
+#include <stdio.h>
+#include <errno.h>
+#include <ifaddrs.h>
+
+int getifaddrs(struct ifaddrs **ifap) {
+    fprintf(stderr, "\n\n\n---------------- dummied out getifaddrs()!\n\n\n");
+    *ifap = NULL;
+    return 0;
+}
+
+void freeifaddrs(struct ifaddrs *ifap) {
+    /* do nothing */
+}
